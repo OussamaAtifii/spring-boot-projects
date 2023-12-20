3988595f6785e5fb46a2bc2939967fd483769be8
OussamaAtifii <oussamaati03@gmail.com>
2023-12-20 21:49:01 +0100

    refactor test class names

--- database-jpa/src/test/java/com/oussama/databasepostgree/TestDataUtil.java
+++ database-jpa/src/test/java/com/oussama/databasepostgree/TestDataUtil.java
@@ -1,57 +1,67 @@
 package com.oussama.databasepostgree;
 
-import com.oussama.databasepostgree.models.Author;
-import com.oussama.databasepostgree.models.Book;
+import com.oussama.databasepostgree.models.dto.AuthorDto;
+import com.oussama.databasepostgree.models.dto.BookDto;
+import com.oussama.databasepostgree.models.entities.AuthorEntity;
+import com.oussama.databasepostgree.models.entities.BookEntity;
 
 public class TestDataUtil {
     private TestDataUtil() {
     }
 
-    public static Author createTestAuthorA() {
-        return Author.builder()
+    public static AuthorEntity createTestAuthorA() {
+        return AuthorEntity.builder()
                 .id(1L)
                 .name("Oussama")
                 .age(25)
                 .build();
     }
 
-    public static Author createTestAuthorB() {
-        return Author.builder()
+    public static AuthorEntity createTestAuthorB() {
+        return AuthorEntity.builder()
                 .id(2L)
                 .name("Pinguino")
                 .age(10)
                 .build();
     }
 
-    public static Author createTestAuthorC() {
-        return Author.builder()
+    public static AuthorEntity createTestAuthorC() {
+        return AuthorEntity.builder()
                 .id(3L)
                 .name("Elefante")
                 .age(34)
                 .build();
     }
 
-    public static Book createTestBookA(Author author) {
-        return Book.builder()
+    public static BookEntity createTestBookA(AuthorEntity authorEntity) {
+        return BookEntity.builder()
                 .isbn("343-1-4324-6756-0")
                 .title("The Lord of the Rings")
-                .author(author)
+                .authorEntity(authorEntity)
                 .build();
     }
 
-    public static Book createTestBookB(Author author) {
-        return Book.builder()
+    public static BookDto createTestBookDtoA(AuthorDto authorDto) {
+        return BookDto.builder()
+                .isbn("343-1-4324-6756-0")
+                .title("The Lord of the Rings")
+                .author(authorDto)
+                .build();
+    }
+
+    public static BookEntity createTestBookB(AuthorEntity authorEntity) {
+        return BookEntity.builder()
                 .isbn("121-1-4324-6756-0")
                 .title("Hello")
-                .author(author)
+                .authorEntity(authorEntity)
                 .build();
     }
 
-    public static Book createTestBookC(Author author) {
-        return Book.builder()
+    public static BookEntity createTestBookC(AuthorEntity authorEntity) {
+        return BookEntity.builder()
                 .isbn("565-1-4324-6756-0")
                 .title("It 2")
-                .author(author)
+                .authorEntity(authorEntity)
                 .build();
     }
 
