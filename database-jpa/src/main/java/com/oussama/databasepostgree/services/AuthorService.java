6a298879767e0d9ca669bb5cfd122cef8346e013
OussamaAtifii <oussamaati03@gmail.com>
2023-12-21 00:19:29 +0100

    add find all and find one

--- database-jpa/src/main/java/com/oussama/databasepostgree/services/AuthorService.java
+++ database-jpa/src/main/java/com/oussama/databasepostgree/services/AuthorService.java
@@ -3,8 +3,15 @@ package com.oussama.databasepostgree.services;
 import com.oussama.databasepostgree.models.entities.AuthorEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public interface AuthorService {
 
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+
+    List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
--- database-jpa/src/main/java/com/oussama/databasepostgree/services/BookService.java
+++ database-jpa/src/main/java/com/oussama/databasepostgree/services/BookService.java
@@ -2,7 +2,14 @@ package com.oussama.databasepostgree.services;
 
 import com.oussama.databasepostgree.models.entities.BookEntity;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookService {
 
     BookEntity createBook(String isbn, BookEntity book);
+
+    List<BookEntity> findAll();
+
+    Optional<BookEntity> findOne(String isbn);
 }
