51040257510ef4a0e7bf66f59bec7f811450e58e
OussamaAtifii <oussamaati03@gmail.com>
2024-02-11 11:28:26 +0100

    Centralize the configuration of microservices in the config microservice

--- SpringMicroservices/microservice-config/src/main/java/com/microservice/config/MicroserviceConfigApplication.java
+++ SpringMicroservices/microservice-config/src/main/java/com/microservice/config/MicroserviceConfigApplication.java
@@ -2,12 +2,14 @@ package com.microservice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MicroserviceConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigApplication.class, args);
+    }
 
 }
