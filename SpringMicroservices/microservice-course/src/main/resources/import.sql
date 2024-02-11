3d45fc2ecd5fb2e4f425b0148586cfb7d4113d8b
OussamaAtifii <oussamaati03@gmail.com>
2024-02-11 11:28:45 +0100

    Add dependencies and import.sql file

---
+++ SpringMicroservices/microservice-course/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO courses (name, teacher)
+VALUES ('Matemáticas', 'Profesor A');
+
+INSERT INTO courses (name, teacher)
+VALUES ('Historia', 'Profesor B');
+
+INSERT INTO courses (name, teacher)
+VALUES ('Ciencias', 'Profesor C');
+
+INSERT INTO courses (name, teacher)
+VALUES ('Literatura', 'Profesor D');
+
+INSERT INTO courses (name, teacher)
+VALUES ('Educación Física', 'Profesor E');
